09951fed936b1274d16aaf2cdd5329c04f4a9fbc
Anthony Gilliam <anthony.gilliam@coxinc.com>
2018-11-16 10:39:50 -0500

    3:12 Hello React - Conditional Rendering in JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,34 +3,50 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
+
+// only render subtitle and p tag if subtitle exists - logical and operator
+// render new p tag - if options has any elements else "No options"
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+};
+
 var template = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        'Anthony'
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'h2',
+        null,
+        app.subtitle
     ),
     React.createElement(
         'p',
         null,
-        'This is some info'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'item 1'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'item 2'
-        )
+        app.options && app.options.length ? 'Here are your options' : 'No options'
     )
 );
+
+var user = {
+    name: 'Anthony',
+    age: 35,
+    location: 'Atlanta'
+};
+
+var getLocation = function getLocation(location) {
+    if (location) return React.createElement(
+        'p',
+        null,
+        'Location: ',
+        location
+    );
+};
 
 // Crate a temlate2 var JSX expression
 var template2 = React.createElement(
@@ -39,20 +55,17 @@ var template2 = React.createElement(
     React.createElement(
         'h1',
         null,
-        'Anthony Gilliam'
+        user.name || 'Anonymous'
     ),
-    React.createElement(
+    user.age && user.age > 18 && React.createElement(
         'p',
         null,
-        'Age: 35'
+        'Age: ',
+        user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        'Location: Atlanta'
-    )
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template2, appRoot);
+ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,26 +1,44 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
+
+// only render subtitle and p tag if subtitle exists - logical and operator
+// render new p tag - if options has any elements else "No options"
+
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+}
+
 var template = (
     <div>
-        <h1>Anthony</h1>
-        <p>This is some info</p>
-        <ol>
-            <li>item 1</li>
-            <li>item 2</li>
-        </ol>
+        <h1>{app.title}</h1>
+        {app.subtitle && <h2>{app.subtitle}</h2>}
+        <p>{(app.options && app.options.length) ? 'Here are your options' : 'No options'}</p>
     </div>
 );
+
+var user = {
+    name: 'Anthony',
+    age: 35,
+    location: 'Atlanta'
+}
+
+var getLocation = (location) => {
+                            if (location)
+                                return <p>Location: {location}</p>
+                        }
 
 // Crate a temlate2 var JSX expression
 var template2 = (
     <div>
-        <h1>Anthony Gilliam</h1>
-        <p>Age: 35</p>
-        <p>Location: Atlanta</p>
+        <h1>{user.name || 'Anonymous'}</h1>
+        {(user.age && user.age > 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template2, appRoot);
+ReactDOM.render(template, appRoot);
