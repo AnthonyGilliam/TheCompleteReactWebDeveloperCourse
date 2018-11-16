d7c9ac09fd0bc022da39f21775185769cd4fb9d8
Anthony Gilliam <anthony.gilliam@coxinc.com>
2018-11-16 09:15:09 -0500

    3:11 Hello React - JSX Expressions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,10 +4,55 @@ console.log('App.js is running!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Anthony'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'item 2'
+        )
+    )
 );
+
+// Crate a temlate2 var JSX expression
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Anthony Gilliam'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 35'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Atlanta'
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,26 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Anthony</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>item 1</li>
+            <li>item 2</li>
+        </ol>
+    </div>
+);
+
+// Crate a temlate2 var JSX expression
+var template2 = (
+    <div>
+        <h1>Anthony Gilliam</h1>
+        <p>Age: 35</p>
+        <p>Location: Atlanta</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
